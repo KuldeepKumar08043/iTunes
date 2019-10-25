57b53f58d2846fd6badaaeced9c8eaf86697d3a6
kuldeep <a_kuldeep3.kumar@gmail.com>
2019-10-25 21:26:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Search from './component/search/Search';
+
+function App(props) {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Application Connects to the iTunes Apple Music</h1>
+      <Search></Search>
     </div>
   );
 }
